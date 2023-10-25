c42b6a638175d2e2e63c8aac913c49362b70f0cd
renanpacheco21 <renanpacheco.delta@gmail.com>
2023-10-25 20:00:10 -0300

    Aula 5

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,29 @@
-var app = angular.module("aplicacao", ['ngResource']);
+var app = angular
+  .module("aplicacao", ["ngResource", "ui.router"])
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state("aplicacao_inicial", {
+        name: "aplicacao_inicial",
+        url: "/inicial.html",
+        templateUrl: "views/inicial.html",
+      })
+      .state("aplicacao_listagem", {
+        name: "aplicacao_listagem",
+        url: "/listagem.html",
+        templateUrl: "views/listagem-alunos.html",
+      })
+      .state("aplicacao_cadastro", {
+        name: "aplicacao_cadastro",
+        url: "/cadastro.html",
+        templateUrl: "views/cadastro-alunos.html",
+      })
+      .state("aplicacao_previsao", {
+        name: "aplicacao_previsao",
+        url: "/previsao.html",
+        templateUrl: "views/previsao-do-tempo.html",
+      });
+  });
+
+app.run(function ($rootScope, $state) {
+  $state.go("aplicacao_inicial");
+});
